edeaef7d34025f5e427d7dd273cf9acf546fcea6
chrisr2 <sirawesome1414@gmail.com>
2016-03-18 18:07:04 -0700

    Post-LA commit
    Everything up to LA regional and 3 new autonomous

---
+++ FRC5012-Code-2016/src/com/gryffingear/y2016/Autonomous/ShovelOfFries.java
@@ -0,0 +1,25 @@
+package com.gryffingear.y2016.Autonomous;
+import com.gryffingear.y2016.Auton.Commands.ArcadeDriveCommand;
+import com.gryffingear.y2016.Auton.Commands.IntakeShooterCommand;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+
+
+public class ShovelOfFries extends CommandGroup {
+	public ShovelOfFries(){
+		
+		this.addSequential(new ArcadeDriveCommand(0.0, 0.0, 2.0));
+		this.addSequential(new ArcadeDriveCommand(0.5, 0.0, 3.0));
+		this.addSequential(new ArcadeDriveCommand(0.0, 0.0, 0.7));
+		this.addSequential(new IntakeShooterCommand(true, false, false, false));
+		this.addSequential(new ArcadeDriveCommand(0.0, 0.0, 0.3));
+		this.addSequential(new ArcadeDriveCommand(-0.3, 0.0, 0.7));
+		this.addSequential(new ArcadeDriveCommand(0.0, 0.0, 0.5));
+		this.addSequential(new IntakeShooterCommand(false, false, false, false));
+		this.addSequential(new ArcadeDriveCommand(1.0, 0.0, 3.0));
+		this.addSequential(new ArcadeDriveCommand(0.0, 0.0, 2.0));
+
+		
+	}
+}
