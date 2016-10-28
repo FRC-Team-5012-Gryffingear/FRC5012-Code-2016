1b4e5f8b36dae577c2e25b27ffbb1d3a3ea1aedb
chrisr2 <sirawesome1414@gmail.com>
2016-10-27 17:47:17 -0700

    post BATB commit
    shooter speed changes at the event + autonomous modes.

--- FRC5012-Code-2016/src/com/gryffingear/y2016/Autonomous/testAuton.java
+++ FRC5012-Code-2016/src/com/gryffingear/y2016/Autonomous/testAuton.java
@@ -1,21 +1,15 @@
 package com.gryffingear.y2016.Autonomous;
 
-import com.gryffingear.y2016.Auton.Commands.ArcadeDriveCommand;
-import com.gryffingear.y2016.Auton.Commands.SetArcadeDrive;
-import com.gryffingear.y2016.Auton.Commands.ShooterCommand;
-import com.gryffingear.y2016.Auton.Commands.StagerCommand;
+import com.gryffingear.y2016.Auton.Commands.DriveStraightCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class testAuton extends CommandGroup {
 
 	public testAuton() {
+		this.addSequential(new DriveStraightCommand(0.75, 2.0));
+		this.addSequential(new DriveStraightCommand(0.5, 1.0));
 		
-		
-		this.addSequential(new ShooterCommand(.8));
-		this.addSequential(new SetArcadeDrive(-.4));
-		this.addSequential(new StagerCommand(-1.0, false, -1.0, 4.0));
-		this.addSequential(new ShooterCommand(0));
 	}
 
 	public String toString() {
